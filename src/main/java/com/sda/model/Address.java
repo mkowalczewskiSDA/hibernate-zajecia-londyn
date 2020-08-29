989edb8183d5003399fca3844ab13c0b53bab623
Michał Kowalczewski <michal.kowalczewski@atos.net>
2020-08-29 10:41:54 +0200

    hibernate

--- src/main/java/com/sda/model/Address.java
+++ src/main/java/com/sda/model/Address.java
@@ -1,7 +1,17 @@
 package com.sda.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Address {
 
-
+  private int id;
+  private String street;
+  private String buildingNo;
+  private String apartamentNo;
+  private String city;
+  private String postalCode;
 
 }
--- src/main/java/com/sda/przyklad1.java
+++ src/main/java/com/sda/przyklad1.java
@@ -1,5 +1,14 @@
 package com.sda;
 
+import com.sda.model.Address;
+
 public class przyklad1 {
+
+  public static void main(String[] args) {
+    //Address address = new Address(1,"a","a","a","a", "a");
+    Address address = new Address();
+
+    System.out.println(address.toString());
+  }
 
 }
