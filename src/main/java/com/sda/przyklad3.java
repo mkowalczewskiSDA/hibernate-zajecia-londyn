cff37b0ec4ac2dd13ff09ba02eb0f24b73950f02
Michał Kowalczewski <michal.kowalczewski@atos.net>
2020-08-30 10:12:10 +0200

    hibernate

--- src/main/java/com/sda/przyklad3.java
+++ src/main/java/com/sda/przyklad3.java
@@ -13,19 +13,19 @@ public class przyklad3 {
   public static void main(String[] args) {
     Session session = HibernateUtil.getSessionFactory().openSession();
     Query<Address> query = session.createQuery("select a from Address a where a.city=:city"
-        , Address.class).setParameter("city", "Londyn").;
+        , Address.class).setParameter("city", "Londyn");
     List<Address> list = query.getResultList();
     list.forEach(System.out::println);
     session.close();
     System.out.println(list.get(0).getCountry().getName());
 
-    /*Query<Address> query1 = session.createQuery("select a from Address a join fetch a.country c where a.city=:city",
+    Query<Address> query1 = session.createQuery("select a from Address a join fetch a.country c where a.city=:city",
         Address.class).setParameter("city", "Londyn");
     List<Address> list2 = query.
         getResultList();
 
-    System.out.println(list2.get(0).getCountry());*/
-    //query1.getResultList().forEach(System.out::println);
+    System.out.println(list2.get(0).getCountry());
+    query1.getResultList().forEach(System.out::println);
 
   }
 
